208ebfb0e626b7169e9d6fdaf787c68824a859fa
Jon Simington <jon.simington@mst.edu>
2020-12-14 22:03:08 -0600

    Add go func to check stock dynamically from config

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -23,6 +29,8 @@ func main() {
 		fmt.Println("error opening connection,", err)
 		return
 	}
+
+	buildMonitors()
 
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
@@ -52,4 +60,97 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Content == "pong" {
 		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
+}
+
+// Monitor Struct
+type Monitor struct {
+	URL               string        `json:"url"`
+	OutOfStockKeyword string        `json:"outOfStockKeyword"`
+	Interval          time.Duration `json:"interval"`
+	FriendlyName      string        `json:"friendlyName"`
+}
+
+// Monitors Struct
+type Monitors struct {
+	Monitors []Monitor `json:"monitors"`
+}
+
+func buildMonitors() {
+	jsonFile, err := os.Open("monitors.json")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Successfully Opened monitors.json")
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var monitors Monitors
+
+	json.Unmarshal(byteValue, &monitors)
+
+	for i := 0; i < len(monitors.Monitors); i++ {
+		monitor := monitors.Monitors[i]
+		interval := monitor.Interval
+		friendlyName := monitor.FriendlyName
+
+		fmt.Printf("Checking %s every %d seconds\n", friendlyName, interval)
+
+		ticker := time.NewTicker(interval * time.Second)
+		quit := make(chan struct{})
+
+		// check stock every 10 seconds
+		go func() {
+			for {
+				select {
+				case <-ticker.C:
+					fmt.Printf("Checking if %s in stock...\n", monitor.FriendlyName)
+
+					start := time.Now()
+					checkStock(monitor)
+					elapsed := time.Since(start)
+					fmt.Printf("Took %s to check %s stock\n", elapsed, monitor.FriendlyName)
+				case <-quit:
+					ticker.Stop()
+					return
+				}
+			}
+		}()
+
+	}
+}
+
+func checkStock(monitor Monitor) {
+	client := &http.Client{}
+
+	userAgent := "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"
+
+	req, err := http.NewRequest("GET", monitor.URL, nil)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.Header.Set("User-Agent", userAgent)
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	html := string(body)
+
+	if strings.Contains(html, monitor.OutOfStockKeyword) {
+		fmt.Printf("%s Out of Stock\n", monitor.FriendlyName)
+	} else {
+		fmt.Printf("%s IN STOCK!!!!!!!!!\n", monitor.FriendlyName)
+	}
+
 }
