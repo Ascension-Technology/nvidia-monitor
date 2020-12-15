c995635edc2e402dd032e2c4df0885b966fcc313
Jon Simington <jon.simington@mst.edu>
2020-12-15 14:06:21 -0600

    Create file to hold discord commands

--- main.go
+++ main.go
@@ -45,26 +45,6 @@ func main() {
 	discord.Close()
 }
 
-// This function will be called when a user sends 'ping' to any subscribed channel
-// so we can make sure the bot is responsive
-func pingPong(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
-
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
-}
-
 func buildMonitors(s *discordgo.Session) {
 	jsonFile, err := os.Open("monitors.json")
 
